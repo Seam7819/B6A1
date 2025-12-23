ce41f978620d0bcb4540911549f124ec2bc8bb0b
Snu Seam <siamahme766@gmail.com>
2025-12-23 22:31:29 +0600

    commit 2nd problem

--- solution.ts
+++ solution.ts
@@ -1,13 +1,26 @@
 //Problem 01
 
-function formatValue(value : number | string | boolean) : number | string | boolean {
-    if(typeof value === 'string'){
+function formatValue(value: number | string | boolean): number | string | boolean {
+    if (typeof value === 'string') {
         return value.toUpperCase();
     }
-    else if( typeof value === 'number'){
+    else if (typeof value === 'number') {
         return value * 10;
-    }else {
+    } else {
         return !value;
     }
-} 
+}
 
+
+// Problem 02
+
+function getLength(value: string | any[]): number {
+    if (Array.isArray(value)) {
+        return value.length;
+    } else if (typeof value === 'string') {
+        return value.length;
+    }
+    {
+        return 0;
+    }
+}
